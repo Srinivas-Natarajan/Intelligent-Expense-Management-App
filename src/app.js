95aed3c8c954d82f14050f7d8a09322fe46b621d
Srinivas Natarajan <nsrinivas06@gmail.com>
2020-12-04 12:34:01 +0530

    Base Routes set up with Nav

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+import AppRouter from './routers/AppRouter';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
 
