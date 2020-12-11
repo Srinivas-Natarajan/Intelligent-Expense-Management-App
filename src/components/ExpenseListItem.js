24a249cea6f2ae2680483482145452bbcad1351b
Srinivas Natarajan <nsrinivas06@gmail.com>
2020-12-11 21:33:23 +0530

    Currency Locales

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import moment from 'moment';
-
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <NavLink to={`/edit/${id}`} activeClassName="is-active" exact={true}>{description}</NavLink>
-    <p>Amount: {amount}</p>
+    <p>Amount: {'\u20B9 ' + numeral(amount).format("Rs 0,0.00")}</p>
     <p>Created At:{moment(createdAt).format("DD MMM, YYYY")}</p>
   </div>
 );
