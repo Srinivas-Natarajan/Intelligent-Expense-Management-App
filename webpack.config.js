4d059c97fb49704a0c392cbfac90d244463f3ab4
Srinivas Natarajan <nsrinivas06@gmail.com>
2020-12-11 19:40:06 +0530

    Setup Dev depnedencies

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -32,6 +32,7 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
+            publicPath: '/dist/',
             historyApiFallback: true
         }
     };
