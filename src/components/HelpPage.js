4cb0e2451c551d01db3c42322b0d6a91721b9cd0
Srinivas Natarajan <nsrinivas06@gmail.com>
2020-12-14 21:58:50 +0530

    Header added to Help page

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const HelpPage = () => (
   <div>
-    This is from my help component
+    <Header />
+    <h4>This is from my help component</h4>
   </div>
 );
 
